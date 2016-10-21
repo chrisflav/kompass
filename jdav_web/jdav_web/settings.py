780e17c811552320b43f23d40dd308098c9fea09
erichhasl <erich.hasl@gmail.com>
2016-10-21 22:46:49 +0200

    localization in url

--- jdav_web/jdav_web/settings.py
+++ jdav_web/jdav_web/settings.py
@@ -128,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Locale files (translations)
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
--- jdav_web/jdav_web/urls.py
+++ jdav_web/jdav_web/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 
-urlpatterns = [
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+print("urlpatterns", urlpatterns)
+
+urlpatterns += i18n_patterns(
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('startpage.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
 # TODO: django serving from MEDIA_URL should be disabled in production stage
 # see http://stackoverflow.com/questions/5871730/need-a-minimal-django-file-upload-example
