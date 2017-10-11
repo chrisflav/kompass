50725290690ae604c87360fd459d26ea15daf69e
erichhasl <erich.hasl@gmail.com>
2017-10-11 21:58:51 +0200

    rename some headings

--- jdav_web/jdav_web/urls.py
+++ jdav_web/jdav_web/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.index_title = _('Startpage')
 
 urlpatterns += i18n_patterns(
     url(r'^admin/', admin.site.urls),
