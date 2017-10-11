7233bd92dfcda28d9fb882dd3cfa369339973215
erichhasl <erich.hasl@gmail.com>
2017-10-11 14:44:08 +0200

    redirect from startpage to dav website

--- jdav_web/startpage/views.py
+++ jdav_web/startpage/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'startpage/index.html')
+    return redirect('http://www.alpenverein-ludwigsburg.de/index.php?id=122')
