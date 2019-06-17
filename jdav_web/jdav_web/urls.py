2a188a49536e0210eae1613b2dae5b324c36d985
Christian Merten <christian@merten-moser.de>
2019-06-17 12:21:26 +0200

    add redirection from admin to kompass url

--- jdav_web/jdav_web/urls.py
+++ jdav_web/jdav_web/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.views.generic.base import RedirectView
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
@@ -26,7 +27,7 @@ admin.site.index_title = _('Startpage')
 
 urlpatterns += i18n_patterns(
     url(r'^kompass/', admin.site.urls),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', RedirectView.as_view(url='/kompass')),
     url(r'^newsletter/', include('mailer.urls', namespace="mailer")),
     url(r'^$', include('startpage.urls')),
 )
