7c86f9a98414916fdeef396d3f0c29a417816372
flavis <christian@flavigny.de>
2022-01-12 20:22:36 +0100

    fix redirect link

--- jdav_web/startpage/views.py
+++ jdav_web/startpage/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    return redirect('http://www.alpenverein-ludwigsburg.de/index.php?id=122')
+    return redirect('https://www.alpenverein-ludwigsburg.de/gruppen/jugendgruppe')
