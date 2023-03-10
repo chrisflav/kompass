4110d58f409422d7e7e46cbc536ba3046a0af663
Christian Merten <christian@merten.dev>
2023-03-10 23:35:11 +0100

    startpage: fix url on dav main website

--- jdav_web/startpage/views.py
+++ jdav_web/startpage/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    return redirect('https://www.alpenverein-ludwigsburg.de/gruppen/jugendgruppe')
+    return redirect('https://www.alpenverein-ludwigsburg.de/gruppen/jugendgruppen')
