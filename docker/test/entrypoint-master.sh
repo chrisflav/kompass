4e3eb7dd5e38edfae5348718e63ddacad85931a5
Christian Merten <christian@merten.dev>
2023-04-10 14:01:36 +0200

    docker: add test setup

---
+++ docker/test/entrypoint-master.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+mysql_ready() {
+cd /app/jdav_web
+python << END
+import sys
+
+from django.db import connections
+from django.db.utils import OperationalError
+
+db_conn = connections['default']
+
+try:
+    c = db_conn.cursor()
+except OperationalError:
+    sys.exit(-1)
+else:
+    sys.exit(0)
+
+END
+}
+
+until mysql_ready; do
+    >&2 echo 'Waiting for MySQL to become available...'
+    sleep 1
+done
+>&2 echo 'MySQL is available'
+
+cd /app
+
+if ! [ -f /tmp/completed_initial_run ]; then
+    echo 'Initialising kompass master container'
+
+    python jdav_web/manage.py compilemessages --locale de
+fi
+
+cd jdav_web
+
+coverage run manage.py test startpage finance members -v 2
