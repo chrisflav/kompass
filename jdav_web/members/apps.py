d512d2b14c02181b9f35fa355f9ae062627ab1ef
Christian Merten <christian@merten.dev>
2024-11-17 17:37:55 +0100

    members: Teilnehmer -> Meine Jugendgruppe

--- jdav_web/members/apps.py
+++ jdav_web/members/apps.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 
 class MembersConfig(AppConfig):
     name = 'members'
-    verbose_name = _('members')
+    verbose_name = _('member administration')
