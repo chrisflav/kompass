3a0df4bc4dc0b9179a540f3398e9e6b30bfa96d5
Christian Merten <christian@merten.dev>
2024-11-17 17:28:51 +0100

    members/echo: ask for dav badge number

--- jdav_web/members/views.py
+++ jdav_web/members/views.py
@@ -14,7 +14,7 @@ class MemberForm(ModelForm):
     class Meta:
         model = Member
         fields = ['prename', 'lastname', 'street', 'plz', 'town', 'address_extra',
-                  'phone_number']
+                  'phone_number', 'dav_badge_no']
 
 class MemberRegistrationForm(ModelForm):
     def __init__(self, *args, **kwargs):
