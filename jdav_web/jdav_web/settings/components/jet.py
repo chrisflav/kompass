bf09be5d19d9f49a573d6aa4d5b7ec822d13ed21
Christian Merten <christian@merten.dev>
2024-12-03 15:43:29 +0100

    settings: reorder sidebar

--- jdav_web/jdav_web/settings/components/jet.py
+++ jdav_web/jdav_web/settings/components/jet.py
@@ -5,6 +5,31 @@ JET_DEFAULT_THEME = 'jdav-green'
 JET_CHANGE_FORM_SIBLING_LINKS = False
 
 JET_SIDE_MENU_ITEMS = [
+    {'label': 'Teilnehmer*innenverwaltung', 'app_label': 'members', 'items': [
+        {'name': 'member', 'label': 'Alle Teilnehmer*innen', 'permissions': ['members.view_member']},
+        {'name': 'freizeit', 'permissions': ['members.view_freizeit']},
+        {'name': 'group', 'permissions': ['members.view_group']},
+        {'name': 'membernotelist', 'permissions': ['members.view_membernotelist']},
+        {'name': 'klettertreff', 'permissions': ['members.view_klettertreff']},
+        {'name': 'activitycategory', 'permissions': ['members.view_group']},
+        {'name': 'trainingcategory', 'permissions': ['members.view_group']},
+    ]},
+    {'label': 'Neue Mitglieder', 'app_label': 'members', 'permissions': ['members.view_memberunconfirmedproxy'], 'items': [
+        {'name': 'memberunconfirmedproxy', 'permissions': ['members.view_memberunconfirmedproxy']},
+        {'name': 'memberwaitinglist', 'permissions': ['members.view_memberwaitinglist']},
+    ]},
+    {'app_label': 'mailer', 'items': [
+        {'name': 'message', 'permissions': ['mailer.view_message']},
+        {'name': 'emailaddress', 'permissions': ['mailer.view_emailaddress']},
+    ]},
+    {'app_label': 'finance', 'items': [
+        {'name': 'statementunsubmitted', 'permissions': ['finance.view_statementunsubmitted']},
+        {'name': 'statementsubmitted', 'permissions': ['finance.view_statementsubmitted']},
+        {'name': 'statementconfirmed', 'permissions': ['finance.view_statementconfirmed']},
+        {'name': 'ledger', 'permissions': ['finance.view_ledger']},
+        {'name': 'bill', 'permissions': ['finance.view_bill', 'finance.view_bill_admin']},
+        {'name': 'transaction', 'permissions': ['finance.view_transaction']},
+    ]},
     {'app_label': 'logindata', 'permissions': ['auth'], 'items': [
         {'name': 'authgroup', 'permissions': ['auth.group'] },
         {'name': 'logindatum', 'permissions': ['auth.user']},
@@ -19,31 +44,6 @@ JET_SIDE_MENU_ITEMS = [
     ]},
     {'app_label': 'ludwigsburgalpin', 'permissions': ['ludwigsburgalpin'], 'items': [
         {'name': 'termin', 'permissions': ['ludwigsburgalpin.view_termin']},
-    ]},
-    {'app_label': 'mailer', 'items': [
-        {'name': 'message', 'permissions': ['mailer.view_message']},
-        {'name': 'emailaddress', 'permissions': ['mailer.view_emailaddress']},
-    ]},
-    {'app_label': 'finance', 'items': [
-        {'name': 'statementunsubmitted', 'permissions': ['finance.view_statementunsubmitted']},
-        {'name': 'statementsubmitted', 'permissions': ['finance.view_statementsubmitted']},
-        {'name': 'statementconfirmed', 'permissions': ['finance.view_statementconfirmed']},
-        {'name': 'ledger', 'permissions': ['finance.view_ledger']},
-        {'name': 'bill', 'permissions': ['finance.view_bill', 'finance.view_bill_admin']},
-        {'name': 'transaction', 'permissions': ['finance.view_transaction']},
-    ]},
-    {'label': 'Teilnehmer*innenverwaltung', 'app_label': 'members', 'items': [
-        {'name': 'member', 'label': 'Alle Teilnehmer*innen', 'permissions': ['members.view_member']},
-        {'name': 'freizeit', 'permissions': ['members.view_freizeit']},
-        {'name': 'group', 'permissions': ['members.view_group']},
-        {'name': 'membernotelist', 'permissions': ['members.view_membernotelist']},
-        {'name': 'klettertreff', 'permissions': ['members.view_klettertreff']},
-        {'name': 'activitycategory', 'permissions': ['members.view_group']},
-        {'name': 'trainingcategory', 'permissions': ['members.view_group']},
-    ]},
-    {'label': 'Neue Mitglieder', 'app_label': 'members', 'permissions': ['members.view_memberunconfirmedproxy'], 'items': [
-        {'name': 'memberunconfirmedproxy', 'permissions': ['members.view_memberunconfirmedproxy']},
-        {'name': 'memberwaitinglist', 'permissions': ['members.view_memberwaitinglist']},
     ]},
     {'app_label': 'material', 'permissions': ['material'], 'items': [
         {'name': 'materialcategory', 'permissions': ['material.view_materialcategory']},
