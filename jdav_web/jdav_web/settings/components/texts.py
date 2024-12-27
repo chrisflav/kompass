7af3ec488e4a76ae10a56438a32663b4be5b2cea
Christian Merten <christian@merten.dev>
2024-12-27 18:31:39 +0100

    members/waitinglist: remove registration form link from invite text
    The link is no longer needed, since a prefilled registration form can
    now be downloaded after filling out the online form.

--- jdav_web/jdav_web/settings/components/texts.py
+++ jdav_web/jdav_web/settings/components/texts.py
@@ -34,9 +34,7 @@ wir haben gute Neuigkeiten für dich. Es ist ein Platz in der Jugendgruppe {grou
 Bitte kontaktiere die Gruppenleitung ({contact_email}) für alle weiteren Absprachen.
 
 Wenn du nach der Schnupperstunde beschließt der Gruppe beizutreten, benötigen wir noch ein paar
-Informationen und deine Anmeldebestätigung von dir. Die lädst du herunter
-(siehe %(REGISTRATION_FORM_DOWNLOAD_LINK)s ), lässt sie von deinen Eltern ausfüllen, unterschreiben
-und lädst ein Foto davon in unserem Anmeldeformular hoch. Das kannst du alles über folgenden Link erledigen:
+Informationen und eine schriftliche Anmeldebestätigung von dir. Das kannst du alles über folgenden Link erledigen:
 
 {{link}}
 
