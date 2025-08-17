88521def1a09a9a75286072dca91ef2a2a9293fb
Christian Merten <christian@merten.dev>
2025-08-17 14:25:59 +0200

    chore(mailer): remove unused subscribe views

--- jdav_web/mailer/urls.py
+++ jdav_web/mailer/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name = "mailer"
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    # url(r'^subscribe', views.subscribe, name='subscribe'),
     re_path(r'^unsubscribe', views.unsubscribe, name='unsubscribe'),
 ]
--- jdav_web/mailer/views.py
+++ jdav_web/mailer/views.py
@@ -53,52 +53,5 @@ def unsubscribe(request):
         return render_confirmation_sent(request, email)
 
 
-def render_subscribe(request, error_message=""):
-    date_input = forms.DateInput(attrs={'required': True,
-                                        'class': 'datepicker',
-                                        'name': 'birthdate'})
-    date_field = date_input.render(_("Birthdate"), "")
-    context = {'date_field': date_field}
-    if error_message:
-        context['error_message'] = error_message
-    return render(request, 'mailer/subscribe.html', context)
-
-
 def render_confirmation_sent(request, email):
     return render(request, 'mailer/confirmation_sent.html', {'email': email})
-
-
-def subscribe(request):
-    try:
-        request.POST['post']
-        try:
-            print("trying to subscribe")
-            prename = request.POST['prename']
-            lastname = request.POST['lastname']
-            email = request.POST['email']
-            print("email", email)
-            birth_date = request.POST['birthdate']
-            print("birthdate", birth_date)
-        except KeyError:
-            return subscribe(request, _("Please fill in every field!"))
-        else:
-            # TODO: check whether member exists
-            exists = Member.objects.filter(prename=prename,
-                                           lastname=lastname)
-            if len(exists) > 0:
-                return render_subscribe(request,
-                                        error_message=_("Member "
-                                                        "already exists"))
-            member = Member(prename=prename,
-                            lastname=lastname,
-                            email=email,
-                            birth_date=birth_date,
-                            gets_newsletter=True)
-            member.save()
-            return subscribed(request)
-    except KeyError:
-        return render_subscribe(request)
-
-
-def subscribed(request):
-    return render(request, 'mailer/subscribed.html')
