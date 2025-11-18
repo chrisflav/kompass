df382b489860eccec1a59172292eddcdd72deb51
Christian Merten <christian@merten.dev>
2025-11-18 22:04:00 +0100

    format mailer

--- jdav_web/mailer/apps.py
+++ jdav_web/mailer/apps.py
@@ -3,5 +3,5 @@ from django.utils.translation import gettext_lazy as _
 
 
 class MailerConfig(AppConfig):
-    name = 'mailer'
-    verbose_name = _('mailer')
+    name = "mailer"
+    verbose_name = _("mailer")
