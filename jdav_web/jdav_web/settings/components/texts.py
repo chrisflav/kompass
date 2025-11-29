20516e92f8a32d592e1d2a56761f7aeefbb80aa1
Christian Merten <christian@merten.dev>
2025-11-29 13:38:11 +0100

    ruff

--- jdav_web/jdav_web/settings/components/texts.py
+++ jdav_web/jdav_web/settings/components/texts.py
@@ -1,17 +1,24 @@
+# ruff: noqa F821
+
 # mail texts
 
-CONFIRM_MAIL_TEXT = get_text('confirm_mail', default="""Hallo {name},
+CONFIRM_MAIL_TEXT = get_text(
+    "confirm_mail",
+    default="""Hallo {{name}},
 
-du hast bei der JDAV %(SEKTION)s eine E-Mail Adresse hinterlegt. Da bei uns alle Kommunikation
-per Email funktioniert, brauchen wir eine Bestätigung {whattoconfirm}. Dazu klicke bitte einfach auf
+du hast bei der JDAV {SEKTION} eine E-Mail Adresse hinterlegt. Da bei uns alle Kommunikation
+per Email funktioniert, brauchen wir eine Bestätigung {{whattoconfirm}}. Dazu klicke bitte einfach auf
 folgenden Link:
 
-{link}
+{{link}}
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION })
+Deine JDAV {SEKTION}""".format(SEKTION=SEKTION),
+)
 
-NEW_UNCONFIRMED_REGISTRATION = get_text('new_unconfirmed_registration', default="""Hallo {name},
+NEW_UNCONFIRMED_REGISTRATION = get_text(
+    "new_unconfirmed_registration",
+    default="""Hallo {name},
 
 für deine Gruppe {group} liegt eine neue unbestätigte Reservierung vor. Die Person hat bereits ihre
 E-Mailadressen bestätigt und ihr Anmeldeformular hochgeladen. Bitte prüfe die Registrierung eingehend und
@@ -20,69 +27,84 @@ bestätige falls möglich. Zu der Registrierung kommst du hier:
 {link}
 
 Viele Grüße
-Dein KOMPASS""")
+Dein KOMPASS""",
+)
 
-GROUP_INVITATION_LEFT_WAITINGLIST = get_text('group_invitation_left_waitinglist',
-                                             default="""Hallo {name},
+GROUP_INVITATION_LEFT_WAITINGLIST = get_text(
+    "group_invitation_left_waitinglist",
+    default="""Hallo {name},
 
 der*die kürzlich zu einer Schnupperstunde für die Gruppe {group} eingeladene Wartende {waiter}
 hat die Warteliste verlassen.
 
 Viele Grüße
-Dein KOMPASS""")
+Dein KOMPASS""",
+)
 
-GROUP_INVITATION_REJECTED = get_text('group_invitation_rejected',
-                                     default="""Hallo {name},
+GROUP_INVITATION_REJECTED = get_text(
+    "group_invitation_rejected",
+    default="""Hallo {name},
 
 {waiter} hat die Einladung zu einer Schnupperstunde bei der Gruppe {group} abgelehnt, ist
 aber weiterhin auf der Warteliste.
 
 Viele Grüße
-Dein KOMPASS""")
+Dein KOMPASS""",
+)
 
 
-GROUP_INVITATION_CONFIRMED_TEXT = get_text('group_invitation_confirmed',
-                                           default="""Hallo {name},
+GROUP_INVITATION_CONFIRMED_TEXT = get_text(
+    "group_invitation_confirmed",
+    default="""Hallo {name},
 
 {waiter} hat die Einladung zu einer Schnupperstunde bei der Gruppe {group} angenommen.
 
 Viele Grüße
-Dein KOMPASS""")
+Dein KOMPASS""",
+)
 
 
-TRIAL_GROUP_MEETING_CONFIRMED_TEXT = get_text('trial_group_meeting_confirmed',
-                                              default="""Hallo {name},
+TRIAL_GROUP_MEETING_CONFIRMED_TEXT = get_text(
+    "trial_group_meeting_confirmed",
+    default="""Hallo {{name}},
 
-deine Teilnahme an der Schnupperstunde der Gruppe {group} wurde erfolgreich bestätigt.
-{timeinfo}
+deine Teilnahme an der Schnupperstunde der Gruppe {{group}} wurde erfolgreich bestätigt.
+{{timeinfo}}
 
 Für alle weiteren Absprachen, kontaktiere bitte die Jugendleiter*innen der Gruppe
-unter {contact_email}.
+unter {{contact_email}}.
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION })
+Deine JDAV {SEKTION}""".format(SEKTION=SEKTION),
+)
 
-GROUP_TIME_AVAILABLE_TEXT = get_text('group_time_available',
-                                     default="""Die Gruppenstunde findet jeden {weekday} von {start_time} bis {end_time} Uhr statt.""")
+GROUP_TIME_AVAILABLE_TEXT = get_text(
+    "group_time_available",
+    default="""Die Gruppenstunde findet jeden {weekday} von {start_time} bis {end_time} Uhr statt.""",
+)
 
-GROUP_TIME_UNAVAILABLE_TEXT = get_text('group_time_unavailable',
-                                       default="""Bitte erfrage die Gruppenzeiten bei der Gruppenleitung ({contact_email}).""")
+GROUP_TIME_UNAVAILABLE_TEXT = get_text(
+    "group_time_unavailable",
+    default="""Bitte erfrage die Gruppenzeiten bei der Gruppenleitung ({contact_email}).""",
+)
 
-INVITE_TEXT = get_text('invite', default="""Hallo {{name}},
+INVITE_TEXT = get_text(
+    "invite",
+    default="""Hallo {{{{name}}}},
 
-wir haben gute Neuigkeiten für dich. Es ist ein Platz in der Jugendgruppe {group_name} {group_link}freigeworden.
-{group_time}
+wir haben gute Neuigkeiten für dich. Es ist ein Platz in der Jugendgruppe {{group_name}} {{group_link}}freigeworden.
+{{group_time}}
 
 Wenn du an der Schnupperstunde teilnehmen möchtest, bestätige deine Teilnahme bitte unter folgendem Link:
 
-{{invitation_confirm_link}}
+{{{{invitation_confirm_link}}}}
 
-Für alle weiteren Absprachen, kontaktiere bitte die Gruppenleitung ({contact_email}).
+Für alle weiteren Absprachen, kontaktiere bitte die Gruppenleitung ({{contact_email}}).
 
 Wenn du nach der Schnupperstunde beschließt der Gruppe beizutreten, benötigen wir noch ein paar
 Informationen und eine schriftliche Anmeldebestätigung von dir. Das kannst du alles über folgenden Link erledigen:
 
-{{link}}
+{{{{link}}}}
 
 Du siehst dort auch die Daten, die du bei deiner Eintragung auf die Warteliste angegeben hast. Bitte
 überprüfe, ob die Daten noch stimmen und ändere sie bei Bedarf ab.
@@ -90,27 +112,35 @@ Du siehst dort auch die Daten, die du bei deiner Eintragung auf die Warteliste a
 Falls du zu dem obigen Termin keine Zeit hast oder dich ganz von der Warteliste abmelden möchtest,
 lehne bitte diese Einladung unter folgendem Link ab:
 
-{{invitation_reject_link}}
+{{{{invitation_reject_link}}}}
 
-Bei Fragen, wende dich gerne an %(RESPONSIBLE_MAIL)s.
+Bei Fragen, wende dich gerne an {RESPONSIBLE_MAIL}.
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION, 'RESPONSIBLE_MAIL': RESPONSIBLE_MAIL,
-                              'REGISTRATION_FORM_DOWNLOAD_LINK': REGISTRATION_FORM_DOWNLOAD_LINK })
+Deine JDAV {SEKTION}""".format(
+        SEKTION=SEKTION,
+        RESPONSIBLE_MAIL=RESPONSIBLE_MAIL,
+    ),
+)
 
 
-LEAVE_WAITINGLIST_TEXT = get_text('leave_waitinglist', default="""Hallo {name},
+LEAVE_WAITINGLIST_TEXT = get_text(
+    "leave_waitinglist",
+    default="""Hallo {{name}},
 
 du hast dich erfolgreich von der Warteliste abgemeldet. Falls du zu einem späteren
 Zeitpunkt wieder der Warteliste beitreten möchtest, kannst du das über unsere Webseite machen.
 
-Falls du dich nicht selbst abgemeldet hast, wende dich bitte umgehend an %(RESPONSIBLE_MAIL)s.
+Falls du dich nicht selbst abgemeldet hast, wende dich bitte umgehend an {RESPONSIBLE_MAIL}.
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION, 'RESPONSIBLE_MAIL': RESPONSIBLE_MAIL })
+Deine JDAV {SEKTION}""".format(SEKTION=SEKTION, RESPONSIBLE_MAIL=RESPONSIBLE_MAIL),
+)
 
 
-WAIT_CONFIRMATION_TEXT = get_text('wait_confirmation', default="""Hallo {name},
+WAIT_CONFIRMATION_TEXT = get_text(
+    "wait_confirmation",
+    default="""Hallo {{name}},
 
 leider können wir dir zur Zeit noch keinen Platz in einer Jugendgruppe anbieten. Da wir
 sehr viele Interessenten haben und wir möglichst vielen die Möglichkeit bieten möchten, an
@@ -119,22 +149,25 @@ Warteliste ab, ob sie noch Interesse haben.
 
 Wenn du weiterhin auf der Warteliste bleiben möchtest, klicke auf den folgenden Link:
 
-{link}
+{{link}}
 
 Falls du kein Interesse mehr hast, kannst du unter folgendem Link die Warteliste verlassen:
 
-{leave_link}
+{{leave_link}}
 
-Das ist Erinnerung Nummer {reminder} von {max_reminder_count}. Nach Erinnerung Nummer {max_reminder_count} wirst
+Das ist Erinnerung Nummer {{reminder}} von {{max_reminder_count}}. Nach Erinnerung Nummer {{max_reminder_count}} wirst
 du automatisch entfernt.
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION })
+Deine JDAV {SEKTION}""".format(SEKTION=SEKTION),
+)
 
 
-JOIN_WAITINGLIST_CONFIRMATION_TEXT = get_text('join_waitinglist_confirmation', default="""Hallo {name},
+JOIN_WAITINGLIST_CONFIRMATION_TEXT = get_text(
+    "join_waitinglist_confirmation",
+    default="""Hallo {{name}},
 
-vielen Dank für dein Interesse an einem Platz in einer Jugendgruppe der JDAV %(SEKTION)s. Du hast dich erfolgreich
+vielen Dank für dein Interesse an einem Platz in einer Jugendgruppe der JDAV {SEKTION}. Du hast dich erfolgreich
 für die Warteliste registriert.
 
 Leider ist die Nachfrage nach Jugendgruppenplätzen deutlich höher als unsere Kapazitäten. Daher kann es
@@ -143,34 +176,43 @@ aktuell bleibt, werden wir dich in regelmäßigen Abständen per E-Mail bitten, 
 zu bestätigen.
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION })
+Deine JDAV {SEKTION}""".format(SEKTION=SEKTION),
+)
 
 
-UNSUBSCRIBE_CONFIRMATION_TEXT = get_text('unsubscribe_confirmation', default="""Klicke auf den Link, um dich vom Newsletter der JDAV %(SEKTION)s abzumelden
+UNSUBSCRIBE_CONFIRMATION_TEXT = get_text(
+    "unsubscribe_confirmation",
+    default="""Klicke auf den Link, um dich vom Newsletter der JDAV {SEKTION} abzumelden
 
-{link}""" % { 'SEKTION': SEKTION })
-
-
-NOTIFY_MOST_ACTIVE_TEXT = get_text('notify_most_active', default="""Hallo {name}!
-
-Herzlichen Glückwunsch, du hast im letzten Jahr zu den {congratulate_max} aktivsten
-Mitgliedern der JDAV %(SEKTION)s gehört! Um genau zu sein beträgt dein Aktivitäts Wert
-des letzten Jahres {score} Punkte. Das entspricht {level} Kletterer:innen. Damit warst du
-im letzten Jahr das {position}aktivste Mitglied der JDAV %(SEKTION)s.
+{{link}}""".format(SEKTION=SEKTION),
+)
 
 
-Auf ein weiteres aktives Jahr in der JDAV %(SEKTION)s.
+NOTIFY_MOST_ACTIVE_TEXT = get_text(
+    "notify_most_active",
+    default="""Hallo {{name}}!
 
-Dein:e Jugendreferent:in""" % { 'SEKTION': SEKTION })
+Herzlichen Glückwunsch, du hast im letzten Jahr zu den {{congratulate_max}} aktivsten
+Mitgliedern der JDAV {SEKTION} gehört! Um genau zu sein beträgt dein Aktivitäts Wert
+des letzten Jahres {{score}} Punkte. Das entspricht {{level}} Kletterer:innen. Damit warst du
+im letzten Jahr das {{position}}aktivste Mitglied der JDAV {SEKTION}.
 
 
-ECHO_TEXT = get_text('echo', default="""Hallo {name},
+Auf ein weiteres aktives Jahr in der JDAV {SEKTION}.
+
+Dein:e Jugendreferent:in""".format(SEKTION=SEKTION),
+)
+
+
+ECHO_TEXT = get_text(
+    "echo",
+    default="""Hallo {{name}},
 
 um unsere Daten auf dem aktuellen Stand zu halten und sicherzugehen, dass du
 weiterhin ein Teil unserer Jugendarbeit bleiben möchtest, brauchen wir eine
 kurze Bestätigung von dir. Dafür besuche einfach diesen Link:
 
-{link}
+{{link}}
 
 Dort kannst du deine Daten nach Eingabe eines Passworts überprüfen und ggf. ändern. Dein
 Passwort ist dein Geburtsdatum. Wäre dein Geburtsdatum zum Beispiel der 4. Januar 1942,
@@ -180,76 +222,94 @@ Falls du nicht innerhalb von 30 Tagen deine Daten bestätigst, gehen wir davon a
 unserer Jugendarbeit sein möchtest. Dein Platz wird dann weitervergeben, deine Daten aus unserer Datenbank
 gelöscht und du erhälst in Zukunft keine Mails mehr von uns.
 
-Bei Fragen, wende dich gerne an %(RESPONSIBLE_MAIL)s.
+Bei Fragen, wende dich gerne an {RESPONSIBLE_MAIL}.
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION, 'RESPONSIBLE_MAIL': RESPONSIBLE_MAIL })
+Deine JDAV {SEKTION}""".format(SEKTION=SEKTION, RESPONSIBLE_MAIL=RESPONSIBLE_MAIL),
+)
 
 
-PREPEND_INCOMPLETE_REGISTRATION_TEXT = get_text('prepend_incomplete_registration', default="""WICHTIGE MITTEILUNG
+PREPEND_INCOMPLETE_REGISTRATION_TEXT = get_text(
+    "prepend_incomplete_registration",
+    default="""WICHTIGE MITTEILUNG
 
 Deine Anmeldung ist aktuell nicht vollständig. Bitte fülle umgehend das
 Anmeldeformular aus und lasse es Deine*r Jugendleiter*in zukommen! Dieses
 kannst Du unter folgendem Link herunterladen:
 
-%(REGISTRATION_FORM_DOWNLOAD_LINK)s
+{REGISTRATION_FORM_DOWNLOAD_LINK}
 
 ****************
 
-""" % { 'REGISTRATION_FORM_DOWNLOAD_LINK': REGISTRATION_FORM_DOWNLOAD_LINK })
+""".format(REGISTRATION_FORM_DOWNLOAD_LINK=REGISTRATION_FORM_DOWNLOAD_LINK),
+)
 
 
-MAIL_FOOTER = get_text('mail_footer', default="""
+MAIL_FOOTER = get_text(
+    "mail_footer",
+    default="""
 
 
 ****************
 
-Diese Email wurde über die Webseite der JDAV %(SEKTION)s
+Diese Email wurde über die Webseite der JDAV {SEKTION}
 verschickt. Wenn Du in Zukunft keine Emails mehr erhalten möchtest,
 kannst Du hier den Newsletter deabonnieren:
 
-{link}""" % { 'SEKTION': SEKTION })
+{{link}}""".format(SEKTION=SEKTION),
+)
 
 
-INVITE_AS_USER_TEXT = get_text('invite_as_user', default="""Hallo {name},
+INVITE_AS_USER_TEXT = get_text(
+    "invite_as_user",
+    default="""Hallo {{name}},
 
-du bist Jugendleiter*in in der Sektion %(SEKTION)s. Die Verwaltung unserer Jugendgruppen,
+du bist Jugendleiter*in in der Sektion {SEKTION}. Die Verwaltung unserer Jugendgruppen,
 Ausfahrten und Finanzen erfolgt in unserer Online Plattform Kompass. Deine Stammdaten sind
 dort bereits hinterlegt. Damit du dich auch anmelden kannst, folge bitte dem folgenden Link
 und wähle ein Passwort.
 
-{link}
+{{link}}
 
-Bei Fragen, wende dich gerne an %(RESPONSIBLE_MAIL)s.
+Bei Fragen, wende dich gerne an {RESPONSIBLE_MAIL}.
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION, 'RESPONSIBLE_MAIL': RESPONSIBLE_MAIL })
+Deine JDAV {SEKTION}""".format(SEKTION=SEKTION, RESPONSIBLE_MAIL=RESPONSIBLE_MAIL),
+)
 
 
-UPLOAD_REGISTRATION_FORM_TEXT = get_text('upload_registration_form', default="""Hallo {name},
+UPLOAD_REGISTRATION_FORM_TEXT = get_text(
+    "upload_registration_form",
+    default="""Hallo {{name}},
 
-vielen Dank für deine Anmeldung in der JDAV %(SEKTION)s. Bevor es richtig losgehen kann, brauchen
+vielen Dank für deine Anmeldung in der JDAV {SEKTION}. Bevor es richtig losgehen kann, brauchen
 wir noch die Bestätigung deiner Daten und die Zustimmung zu unseren Teilnahmebedingungen.
 
 Dafür kannst du das für dich vorausgefüllte Anmeldeformular unter folgendem Link herunterladen,
 durchlesen und, falls du zustimmst, das unterschriebene Formular wieder dort hochladen.
 
-{link}
+{{link}}
 
 Bist du noch nicht volljährig? Dann muss eine erziehungsberechtigte Person das Formular unterschreiben.
 
-Bei Fragen, wende dich gerne an %(RESPONSIBLE_MAIL)s.
+Bei Fragen, wende dich gerne an {RESPONSIBLE_MAIL}.
 
 Viele Grüße
-Deine JDAV %(SEKTION)s""" % { 'SEKTION': SEKTION, 'RESPONSIBLE_MAIL': RESPONSIBLE_MAIL })
+Deine JDAV {SEKTION}""".format(SEKTION=SEKTION, RESPONSIBLE_MAIL=RESPONSIBLE_MAIL),
+)
 
 
-ADDRESS = get_text('address', default="""JDAV %(SEKTION)s
-%(STREET)s
-%(PLACE)s""" % { 'SEKTION': SEKTION, 'STREET': SEKTION_STREET, 'PLACE': SEKTION_TOWN })
+ADDRESS = get_text(
+    "address",
+    default="""JDAV {SEKTION}
+{STREET}
+{PLACE}""".format(SEKTION=SEKTION, STREET=SEKTION_STREET, PLACE=SEKTION_TOWN),
+)
 
 
-NOTIFY_EXCURSION_PARTICIPANT_LIST = get_text('notify_excursion_participant_list', default="""Hallo {name},
+NOTIFY_EXCURSION_PARTICIPANT_LIST = get_text(
+    "notify_excursion_participant_list",
+    default="""Hallo {name},
 
 deine Ausfahrt {excursion} steht kurz bevor. Damit die Sektion dich im Notfall gut unterstützen kann, benötigt
 die Geschäftsstelle eine aktuelle Kriseninterventionsliste, das heißt eine Teilnehmendenliste der Ausfahrt.
@@ -262,9 +322,12 @@ Das sind die aktuell in der Ausfahrt eingetragenen Teilnehmenden:
 Falls diese Liste nicht mehr aktuell ist, gehe bitte umgehend auf {excursion_link} und trage die Daten nach.
 
 Viele Grüße
-Dein KOMPASS""")
+Dein KOMPASS""",
+)
 
-SEND_EXCURSION_CRISIS_LIST = get_text('send_excursion_crisis_list', default="""Hallo zusammen,
+SEND_EXCURSION_CRISIS_LIST = get_text(
+    "send_excursion_crisis_list",
+    default="""Hallo zusammen,
 
 vom {excursion_start} bis {excursion_end} findet die Ausfahrt {excursion} der Jugend statt. Die
 Ausfahrt wird geleitet von {leaders}.
@@ -272,12 +335,16 @@ Ausfahrt wird geleitet von {leaders}.
 Im Anhang findet ihr die Kriseninterventionsliste.
 
 Viele Grüße
-Euer KOMPASS""")
+Euer KOMPASS""",
+)
 
-SEND_STATEMENT_SUMMARY = get_text('send_statement_summary', default="""Hallo zusammen,
+SEND_STATEMENT_SUMMARY = get_text(
+    "send_statement_summary",
+    default="""Hallo zusammen,
 
 anbei findet ihr die Abrechnung inklusive Belege für {statement}. Die Überweisungen
 wurden wie beschrieben ausgeführt.
 
 Viele Grüße
-Euer KOMPASS""")
+Euer KOMPASS""",
+)
