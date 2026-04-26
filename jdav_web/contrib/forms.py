da328376c56399046126e1349d464398b58f737a
Christian Merten <christian@merten.dev>
2026-04-27 00:33:43 +0200

    remove space

--- jdav_web/contrib/forms.py
+++ jdav_web/contrib/forms.py
@@ -1,5 +1,5 @@
 class RequiredFieldsMixin:
-    """Mixin that appends ' *' to the label of every required field.
+    """Mixin that appends '*' to the label of every required field.
 
     Accounts for fields made required via Meta.required in addition to
     fields that are required by the model definition.
@@ -10,4 +10,4 @@ class RequiredFieldsMixin:
         meta_required = set(getattr(getattr(self, "Meta", None), "required", []))
         for name, field in self.fields.items():
             if field.required or name in meta_required:
-                field.label = f"{field.label} *"
+                field.label = f"{field.label}*"
